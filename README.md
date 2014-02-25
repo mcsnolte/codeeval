7822441cac19408342ed53d82607ebeeb664778b
Steve Nolte <steve.nolte@corvisa.com>
2014-02-25 13:02:50 -0600

    Add template.pl and update readme

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ codeeval
 ========
 
 CodeEval solutions
+
+I plan on doing as many as I can in Perl, but hopefully some in Python as well.
---
+++ template.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+
+=head1 DESCRIPTION
+
+=cut
+
+use strict;
+use warnings;
+
+while (<DATA>) {
+}
+exit(0);
+
+__DATA__
